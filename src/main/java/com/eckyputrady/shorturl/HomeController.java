a7983892911808be070659a27e772993ec674a14
Ecky Putrady <eckyputrady@gmail.com>
2016-09-17 11:12:34 +0700

    Receive webhook

--- src/main/java/com/eckyputrady/shorturl/HomeController.java
+++ src/main/java/com/eckyputrady/shorturl/HomeController.java
@@ -1,10 +1,14 @@
 package com.eckyputrady.shorturl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@Slf4j
 public class HomeController {
 
     @Value("${USER_AGENT:none}")
@@ -18,5 +22,15 @@ public class HomeController {
     @RequestMapping("/useragent")
     public String getUserAgent() {
         return userAgent;
+    }
+
+    @RequestMapping("/github/webhook")
+    public String githubWebhook(
+            @RequestHeader(value = "X-GitHub-Event") String event,
+            @RequestHeader(value = "X-GitHub-Delivery") String id,
+            @RequestBody String payload
+    ) {
+        log.info("ID={}, Event={}, Payload={}", id, event, payload);
+        return "";
     }
 }
